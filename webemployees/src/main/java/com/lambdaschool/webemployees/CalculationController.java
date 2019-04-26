b613e7680b6332047a81e2c5120ca6ee0221e2e6
jrmmba8314 <jrmmba@outlook.com>
2019-04-26 08:35:18 -0700

    Updating

--- webemployees/src/main/java/com/lambdaschool/webemployees/CalculationController.java
+++ webemployees/src/main/java/com/lambdaschool/webemployees/CalculationController.java
@@ -1,5 +1,7 @@
 package com.lambdaschool.webemployees;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,12 +11,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/calc")
 public class CalculationController
 {
-    @RequestMapping("/salary")
-    public Employee checkRaise(@RequestParam(value = "id") long id,
-                               @RequestParam(value = "raise") double raise)
+    @RequestMapping(value = "/salary",
+                    produces = {"application/json"})
+    public ResponseEntity<?> checkRaise(
+            @RequestParam(value = "id")
+                    long id,
+            @RequestParam(value = "raise")
+                    double raise)
     {
-        Employee tempEmp = new Employee(WebemployeesApplication.ourEmpList.findEmployee(e-> (e.getId() == id)));
+
+        // the following line is wrong
+        // Employee tempEmp = WebemployeesApplication.ourEmpList.findEmployee(e -> (e.getId() == id));
+
+        // the following line is correct
+        Employee tempEmp = new Employee(WebemployeesApplication.ourEmpList.findEmployee(e -> (e.getId() == id)));
         tempEmp.setSalary(tempEmp.getSalary() * (1.0 + raise));
-        return tempEmp;
+        return new ResponseEntity<>(tempEmp, HttpStatus.OK);
     }
 }
--- webemployees/src/main/java/com/lambdaschool/webemployees/EmployeeController.java
+++ webemployees/src/main/java/com/lambdaschool/webemployees/EmployeeController.java
@@ -1,29 +1,46 @@
 package com.lambdaschool.webemployees;
 
-//localhost:8080/data/allemployees
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.ArrayList;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/data")
 public class EmployeeController
 {
-    @RequestMapping("/employee")
-    public Employee getEmployeeDetail(@RequestParam(value="id") long id)
+
+    // localhost:8080/data/employee?id=2
+    @RequestMapping(value = "/employee", method = RequestMethod.GET, produces={"application/json"})
+    public ResponseEntity<?> getEmployeeDetail(@RequestParam("id") long id)
     {
-        return WebemployeesApplication.ourEmpList.findEmployee(e-> (e.getId() == id));
+        Employee rtnEmp = WebemployeesApplication.ourEmpList.findEmployee(e-> (e.getId() == id));
+        return new ResponseEntity<>(rtnEmp, HttpStatus.OK);
+    }
+
+    // localhost:8080/data/employee?id=2
+    @GetMapping(value = "/employee", produces={"application/json"})
+    public ResponseEntity<?> getEmployeeDetail2(@RequestParam("id") long id)
+    {
+        Employee rtnEmp = WebemployeesApplication.ourEmpList.findEmployee(e-> (e.getId() == id));
+        return new ResponseEntity<>(rtnEmp, HttpStatus.OK);
     }
 
 
-    @RequestMapping("/allemployees")
-    public ArrayList<Employee> getAllEmployees()
+    // localhost:8080/data/employee/2
+    @GetMapping(value = "/employee/{id}", produces = {"application/json"})
+    public ResponseEntity<?> getEmpDetail(@PathVariable long id)
+    {
+        Employee rtnEmp = WebemployeesApplication.ourEmpList.findEmployee(e-> (e.getId() == id));
+        return new ResponseEntity<>(rtnEmp, HttpStatus.OK);
+    }
+
+
+    //localhost:8080/data/allemployees
+    @RequestMapping(value = "/allemployees", produces = {"application/json"})
+    public ResponseEntity<?> getAllEmployees()
     {
         WebemployeesApplication.ourEmpList.empList.sort((e1, e2) ->
                 e1.getName().compareToIgnoreCase(e2.getName()));
-        return WebemployeesApplication.ourEmpList.empList;
+        return new ResponseEntity<>(WebemployeesApplication.ourEmpList.empList, HttpStatus.OK);
     }
 }
