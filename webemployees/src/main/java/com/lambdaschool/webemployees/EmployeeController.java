90cf625dc905a8c11a240a0871d0441b01c54da5
jrmmba8314 <jrmmba@outlook.com>
2019-07-15 12:22:51 -0700

    Removing non-rest end points

--- webemployees/src/main/java/com/lambdaschool/webemployees/EmployeeController.java
+++ webemployees/src/main/java/com/lambdaschool/webemployees/EmployeeController.java
@@ -2,7 +2,10 @@ package com.lambdaschool.webemployees;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 
@@ -11,26 +14,12 @@ import java.util.ArrayList;
 public class EmployeeController
 {
     //localhost:8080/data/allemployees
-    @RequestMapping(value = "/allemployees",
-                    produces = {"application/json"})
+    @GetMapping(value = "/allemployees",
+                produces = {"application/json"})
     public ResponseEntity<?> getAllEmployees()
     {
-        WebemployeesApplication.ourEmpList.empList.sort((e1, e2) -> e1.getName().compareToIgnoreCase(e2.getName()));
+        WebemployeesApplication.ourEmpList.empList.sort((e1, e2) -> e1.getFname().compareToIgnoreCase(e2.getFname()));
         return new ResponseEntity<>(WebemployeesApplication.ourEmpList.empList, HttpStatus.OK);
-    }
-
-
-    // the web way
-    // localhost:8080/data/employee?id=2
-    @RequestMapping(value = "/employee",
-                    method = RequestMethod.GET,
-                    produces = {"application/json"})
-    public ResponseEntity<?> getEmployeeDetail(
-            @RequestParam("id")
-                    long id)
-    {
-        Employee rtnEmp = WebemployeesApplication.ourEmpList.findEmployee(e -> (e.getId() == id));
-        return new ResponseEntity<>(rtnEmp, HttpStatus.OK);
     }
 
 
@@ -55,6 +44,19 @@ public class EmployeeController
     {
         ArrayList<Employee> rtnEmps = WebemployeesApplication.ourEmpList.
                 findEmployees(e -> e.getFname().toUpperCase().charAt(0) == Character.toUpperCase(letter));
+        return new ResponseEntity<>(rtnEmps, HttpStatus.OK);
+    }
+
+    // localhost:8080/data/employees/s
+    @GetMapping(value = "/sortedemployees/{letter}",
+                produces = {"application/json"})
+    public ResponseEntity<?> getSortedEmployees(
+            @PathVariable
+                    char letter)
+    {
+        ArrayList<Employee> rtnEmps = WebemployeesApplication.ourEmpList.
+                findEmployees(e -> e.getFname().toUpperCase().charAt(0) == Character.toUpperCase(letter));
+        rtnEmps.sort((e1, e2) -> ((int) (e1.getSalary() - e2.getSalary())));
         return new ResponseEntity<>(rtnEmps, HttpStatus.OK);
     }
 
